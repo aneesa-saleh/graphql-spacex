417ba3351be8d992c5110e7813e68d4fb8270f14
Aneesa Saleh <aneesa.saleh@gmail.com>
2019-07-30 06:16:54 +0100

    Added react apollo to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import Launches from './components/Launches';
 import './App.css';
+import logo from './spacex.png';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ApolloProvider client={client}>
+      <div className="App container">
+      <img
+        src={logo} alt="SpaceX"
+        style={{width: "200px", margin: "30px 0" }}
+      />
+      <Launches />
     </div>
+    </ApolloProvider>
   );
 }
 
