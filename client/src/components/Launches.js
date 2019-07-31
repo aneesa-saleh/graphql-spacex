cd5133b3475332a06acca95fb075227719e5d746
Aneesa Saleh <aneesa.saleh@gmail.com>
2019-07-31 06:19:03 +0100

    Separate render launches logic into a function

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -14,27 +14,30 @@ const LAUNCHES_QUERY = gql`
   }
 `;
 
+const renderLaunches = ({ loading, error, data }) => {
+  if (loading) {
+    return <h4>Loading...</h4>;
+  } else if (error) {
+    console.error(error);
+    return <h4 style={{ color: "red" }}>An error occurred.</h4>;
+  } else {
+    return (
+      <Fragment>
+        {data.launches.map(launch => (
+          <LaunchItem key={launch.flight_number} launch={launch} />
+        ))}
+      </Fragment>
+    );
+  }
+};
+
 export class Launches extends Component {
   render() {
     return (
       <Fragment>
         <h1 className="display-4 my-3">Launches</h1>
         <Query query={LAUNCHES_QUERY}>
-          {({ loading, error, data }) => {
-            if (loading) {
-              return <h4>Loading...</h4>;
-            } else if (error) {
-              console.error(error);
-            } else {
-              return (
-                <Fragment>
-                  {data.launches.map(launch => (
-                    <LaunchItem key={launch.flight_number} launch={launch} />
-                  ))}
-                </Fragment>
-              );
-            }
-          }}
+          {renderLaunches}
         </Query>
       </Fragment>
     );
