ff5c40be811ba3e0d782ddaa9a968f3d9b26fdda
Aneesa Saleh <aneesa.saleh@gmail.com>
2019-08-02 05:51:14 +0100

    Add deployment configuration

---
+++ public/precache-manifest.8eef2bd400da4ce4c9456fca847a61d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2da4d98e4a2e84361ff3060606863a04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8fdbbf54a45ff5cd7cb",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5286bd84c63fd42a1d83",
+    "url": "/static/js/2.6aa36e9c.chunk.js"
+  },
+  {
+    "revision": "c8fdbbf54a45ff5cd7cb",
+    "url": "/static/js/main.ee10caae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e6636a81622ea279a8a58f067568a40c",
+    "url": "/static/media/spacex.e6636a81.png"
+  }
+]);
